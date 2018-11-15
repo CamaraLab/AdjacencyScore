18b028c3e975ec7da2b3331126ec74a14a7ac171
Kiya Govek <govekk@pennmedicine.upenn.edu>
2018-11-15 11:18:56 -0500

    changed permutation method of overlap score

--- R/overlap_score.R
+++ R/overlap_score.R
@@ -1,14 +1,9 @@
-#' Ranks pairs of features using the Combinatorial Laplacian Score for 0- and 1-forms.
+#' Ranks pairs of features by how much they overlap in a graph
 #'
-#' Given a nerve or a clique complex, a set of features consisting of functions with support on
-#' the set of points underlying the complex, and a list of pairs of features,
-#' it asseses the significance of each pair of features
-#' in the simplicial complex by computing its scalar and vectorial Combinatorial Laplacian
-#' Score and comparing it with the null distribution that results from reshufling many times the values of
-#' the function across the point cloud. For nerve complexes, feature functions induce 0- and
-#' 1-forms in the complex by averaging the function across the points associated to 0- and 1-simplices
-#' respectively. For clique complexes, feature functions are directly 0-forms in the complex and 1-forms
-#' are obtained by averaging the function across the two vertices connected by each edge.
+#' Given the adjacency matrix of a graph and a set of features on that graph, ranks given pairs
+#' of those features (f and g) by the equation fg, which measures how much those features
+#' cooccur in the same cells. Calculates the p-value for this score by permuting
+#' the columns of the feature matrix separately for each features.
 #'
 #' @param f a numeric vector or matrix specifying one or more functions with support on
 #' the set of points whose significance will be assesed in the simplicial complex. Each
@@ -21,6 +16,8 @@
 #' By default is set to 10.
 #' @param num_cores integer specifying the number of cores to be used in the computation. By
 #' default only one core is used.
+#' @param perm_estimate boolean indicating whether normal distribution parameters should be
+#' determined from num_perms permutations to estimate the p-value. By default is set to FALSE.
 #'
 #' @import Matrix
 #' @import parallel
@@ -28,7 +25,7 @@
 #' @import MASS
 #' @export
 
-overlap_score <- function(f, f_pairs, num_perms = 1000, seed = 10, num_cores = 1) {
+overlap_score <- function(f, f_pairs, num_perms = 1000, seed = 10, num_cores = 1, perm_estimate = F) {
 
   # Check class of f
   if (class(f) != 'matrix') {
@@ -43,23 +40,80 @@ overlap_score <- function(f, f_pairs, num_perms = 1000, seed = 10, num_cores = 1
     f_pairs <- matrix(f_pairs, ncol=2, byrow=T)
   }
 
-  qqh <- NULL
-  for (i in 1:nrow(f_pairs)) {
-    f1 <- as.numeric(f[f_pairs[i,1],])
-    f2 <- as.numeric(f[f_pairs[i,2],])
+  perm_f <- vector('list', nrow(f))
+  for (i in 1:nrow(f)) {
+    permutations <- NULL
     if (num_perms > 0) {
-      perm1 <- t(mcmapply(function(x) f1[sample(1:ncol(f))], 1:num_perms, mc.cores=num_cores))
-      perm2 <- t(mcmapply(function(x) f2[sample(1:ncol(f))], 1:num_perms, mc.cores=num_cores))
-      f1 <- rbind(f1, perm1)
-      f2 <- rbind(f2, perm2)
+      permutations <- t(mcmapply(function(x) sample(1:ncol(f)), 1:num_perms, mc.cores=num_cores))
+    }
+    permutations <- rbind(1:ncol(f), permutations)
+    perm_f[[i]] <-  t(sapply(1:nrow(permutations), function(j) f[i,][permutations[j,]]))
+  }
+  names(perm_f) <- row.names(f)
+
+  # Evaluates R and p for a pair of features fo
+  cornel <- function(fo) {
+    f1 <- perm_f[[fo[1]]]
+    f2 <- perm_f[[fo[2]]]
+    if (num_perms > 0) {
       qt <- rowSums(f1*f2)
     } else {
       qt <- sum(f1 * f2)
     }
-    ph <- c( score = qt[1], p = (sum(qt>=qt[1])-1.0)/num_perms)
-    qqh <- rbind(qqh, ph)
+    ph <- NULL
+    ph$score <- qt[1]
+    if (perm_estimate) {
+      nfit <- fitdistr(as.numeric(qt), "normal")
+      ph$p <- 1-pnorm(qt[1], mean=nfit$estimate["mean"], sd = nfit$estimate["sd"])
+    }
+    else {
+      ph$p <- (sum(qt>=qt[1])-1.0)/num_perms
+    }
+    return(ph)
   }
-  qqh <- as.data.frame(qqh, row.names=1:nrow(qqh))
+
+  # Each worker evaluates R anp p for a set fu of pairs of features
+  worker <- function(fu) {
+    qh <- NULL
+    qh$score <- NULL
+    qh$p <- NULL
+    for (i in 1:nrow(fu)) {
+      d <- cornel(fu[i,])
+      qh$score <- rbind(qh$score, d$score)
+      qh$p <- rbind(qh$p, d$p)
+    }
+    return(data.frame(qh))
+  }
+
+  if (num_cores > nrow(f_pairs)) {
+    num_cores <- nrow(f_pairs)
+  }
+
+  if (num_cores == 1 || nrow(f_pairs) == 1) {
+    qqh <- worker(f_pairs)
+  } else {
+    # If more than one core then split the features in num_cores parts accordingly
+    wv <- floor(nrow(f_pairs)/num_cores)
+    wr <- nrow(f_pairs) - wv*num_cores
+    work <- list()
+    if (wr>0) {
+      for (m in 1:wr) {
+        work[[m]] <- (f_pairs[(1+(m-1)*(wv+1)):(m*(wv+1)),,drop=FALSE])
+      }
+      for (m in (wr+1):num_cores) {
+        work[[m]] <- (f_pairs[(1+wr+(m-1)*wv):(wr+m*wv),,drop=FALSE])
+      }
+    } else {
+      for (m in 1:num_cores) {
+        work[[m]] <- (f_pairs[(1+(m-1)*wv):(m*wv),,drop=FALSE])
+      }
+    }
+    reul <- mclapply(work, worker, mc.cores = num_cores)
+    qqh <- reul[[1]]
+    for (m in 2:num_cores) {
+      qqh <- rbind(qqh, reul[[m]])
+    }
+  }
 
   # Adjust for multiple hypothesis testing
   qqh$q <- p.adjust(qqh$p, method = 'BH')
